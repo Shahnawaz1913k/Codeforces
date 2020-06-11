dacbadc70f63d05fba38b8f8fd45ab51db946b57
Basukinath Tiwari <basukinathtiwari231198@gmail.com>
2020-06-11 16:58:03 +0530

    Added Problems

--- 225C.cpp
+++ 225C.cpp
@@ -2,34 +2,23 @@
 using namespace std;
 typedef long long int ll;
 
-ll has[2000], dot[2000], dp[2000][2];
+ll a[100100], up[100100], down[100100];
 
 void solve(){
-    ll n, m, x, y;
-    cin >> n >> m >> x >> y;
-    string s[n];
-    for(ll i = 0; i < n; i++) cin >> s[i];
-    for(ll i = 0; i < m; i++) {
-        for(ll j = 0; j < n; j++) s[j][i] == '#' ? has[i+1]++: dot[i+1]++;
+    ll n, m;
+    cin >> n >> m;
+    for(ll i = 1; i <= n; i++) {
+        cin >> a[i];
+        if(a[i-1] < a[i] && i > 1) up[i-1] = up[i-2] + 1;
+    } up[n] = up[n-1];
+    for(ll i = n; i >= 2; i--) {
+        if(a[i-1] > a[i]) down[i] = down[i+1] + 1;
+    } down[1] = down[2];
+    while(m--){
+        ll l, r, cnt = 0, mid;
+        cin >> l >> r;
+        
     }
-    for(ll i = 1; i <= m; i++) {
-        has[i] += has[i-1];
-        dot[i] += dot[i-1];
-        dp[i][0] = 999999999999;
-        dp[i][1] = 999999999999;
-    }
-    dp[1][0] = dp[1][1] = 0;
-    for(ll i = 1; i <= m; i++) {
-        ll xx = 99999999999999, yy = 999999999999;
-        for(ll j = max(0ll, i - y); j <= i - x; ++j){
-            //cout << j <<" |  " << has[i] << " " << has[j] <<  endl;
-            xx = min(xx, dp[j][1] + has[i] - has[j]);
-            yy = min(yy, dp[j][0] + dot[i] - dot[j]);
-        }
-        //cout << i << " " << xx << " " << yy << endl;
-        dp[i][0] = xx, dp[i][1] = yy;
-    }
-    cout << min(dp[m][0] , dp[m][1]) << endl;
 }
 
 int main(){
