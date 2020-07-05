954a4deb9142819b859d785bfd308fe891e8dda0
Basukinath Tiwari <basukinathtiwari231198@gmail.com>
2020-07-05 09:14:45 +0530

    Update e.cpp

--- 1373/e.cpp
+++ 1373/e.cpp
@@ -3,19 +3,34 @@ using namespace std;
 typedef long long int ll;
 
 void solve(){
-    ll n, m;
-    cin >> n >> m;
-    pair<ll, ll> a[n+1];
-    for(ll i = 1; i <= n; i++) a[i] = {0, i};
-    while(m--){
-        ll x, y;
-        cin >> x >> y;
-        a[y].first--;
+    ll n, l;
+    cin >> n >> l;
+    string s, st[2*n - 1];
+    for(ll i = 0; i < 2*n - 1; i++)
+        for(ll j = 0; j < 2*n - 1; j++) st[i] += '*';
+    cin >> s;
+    ll cnt = 0;
+    for(ll i = n-1; i >= 0; i--){
+        if(i == 0) st[n-1][n-1] = s[cnt];
+        else {
+            for(ll j = 0; j < i; j++) { st[n-1-j][j+(n-1-i)] = s[cnt]; 
+                cnt = (cnt+1)%l;
+            }
+            for(ll j = 0; j < i; j++) {
+                st[j+(n-1-i)][n-1+j] = s[cnt], cnt = (cnt+1)%l;
+            }
+            for(ll j = 0; j < i; j++) {
+                st[n-1+j][2*(n-1)-j-(n-1-i)] = s[cnt], cnt = (cnt+1)%l;
+            }
+            for(ll j = 0; j < i; j++) {
+                st[2*(n-1)-j-(n-1-i)][n-1-j] = s[cnt], cnt = (cnt+1)%l;
+            }
+        }
     }
-    sort(a + 1, a+n+1);
-    cout << 4*n/7 << endl;
-    for(ll i = 1; i <= 4*n/7; i++) cout << a[i].second << " ";
-    cout << endl;
+    for(ll i = 0; i < 2*n - 1; i++) {
+        for(ll j = 0; j < 2*n - 1; j++) cout << (st[i][j] == '*' ? ' ':st[i][j]);
+        cout << endl;
+    }
 }
 
 
