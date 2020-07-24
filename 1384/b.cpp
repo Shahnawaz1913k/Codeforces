8fe2283d18408e89691def6537591c2cfcda3b6d
Basukinath Tiwari <basukinathtiwari231198@gmail.com>
2020-07-24 23:30:33 +0530

    Create b.cpp

---
+++ 1384/b.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void solve(){
+    ll n, k, l;
+    cin >> n >> k >> l;
+    ll a[n], t[2*k];
+    for(ll i = 0; i < n; i++) cin >> a[i];
+    for(ll i = 0; i <= k; i++) t[i] = i;
+    for(ll i = 1; i <= k ;i++) t[2*k-i] = i;
+
+    ll pos = k;
+    for(ll i = 0; i < n; i++) {
+        //cout << i << " " << pos << " " << a[i] << " " << pos%(2*k) << endl;
+        if(a[i] > l){
+            cout << "no" << endl;
+            return;
+        }
+        if(a[i] + k <= l) pos = k;
+        else {
+            if(pos%(2*k) >= k) { 
+                while(t[pos%(2*k)] + a[i] > l) pos++;
+            } else if(t[pos%(2*k)+1] + a[i] > l){
+               cout << "no"  << endl;
+               return;
+            } else pos++;
+        }
+    }
+    cout << "yes" << endl;
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int t;cin>>t;while(t--)
+  solve();
+  return 0;
+}
