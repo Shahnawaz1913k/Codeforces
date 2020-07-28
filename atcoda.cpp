70e1e1196a83e1f276563b71fe70fe3a190752e4
Basukinath Tiwari <basukinathtiwari231198@gmail.com>
2020-07-28 21:13:57 +0530

    Update atcoda.cpp

--- atcoda.cpp
+++ atcoda.cpp
@@ -2,20 +2,38 @@
 using namespace std;
 typedef long long int ll;
 
-void solve(){
-    ll n, k;
+void solve(int x){
+    ll n;
     cin >> n;
-    ll g = 0, ma = 0;
-    ll a[n];
-    for(ll i = 0; i < n; i++) cin >> a[i], g = __gcd(g, a[i]), ma = max(ma, a[i]);
-    cin >> k;
-    cout << (k*g > ma ? -1:ma - (k-1)*g) << endl;
+    string in, out;
+    cin >> in >> out;
+    string s[n];
+    for(ll i = 0; i < n; i++){
+        for(ll j = i; j < n; j++){
+            if(i == j) {
+                //s[i][j] = (out[i] == 'Y' ? 'Y':'N');
+                s[i][i] = 'Y';
+                continue;
+            }
+            s[i][j] = (out[j-1] == 'Y' && s[i][j-1] == 'Y' && in[j] == 'Y' ? 'Y':'N');
+        }
+        for(ll j = i-1; j >= 0; --j){
+            s[i][j] = (out[j+1] == 'Y' && s[i][j+1] == 'Y' && in[j] == 'Y' ? 'Y':'N');
+        }
+    }
+    cout << "Case #"<<x+1<<":"<<endl;
+    for(ll i = 0; i < n; i++){
+        for(ll j = 0; j < n; j++) cout << s[i][j];
+        cout << endl;
+    }
 }
 
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(0);
-  int t;cin>>t;while(t--)
-  solve();
+  int t;cin>>t;
+  //while(t--)
+  for(int i = 0; i < t; i++)
+  solve(i);
   return 0;
 }
