4e9311f8d2f45640a3c986f0b68daf98919c5d89
Basukinath Tiwari <basukinathtiwari231198@gmail.com>
2020-07-28 21:13:46 +0530

    Update d.cpp

--- 1380/d.cpp
+++ 1380/d.cpp
@@ -2,39 +2,50 @@
 using namespace std;
 typedef long long int ll;
 
+ll check(vector<ll> v, ll ma, ll x, ll k, ll y){
+    ll n = v.size(), cnt = 0;
+    if(!n) return 0;
+    sort(v.begin(), v.end());
+    if(v[n-1] > ma && n < k) return -1;
+    bool val = (v[n-1] < ma ? true:false);
+    cnt += (n%k)*y;
+    n -= n%k;
+    if(!n) return cnt;
+    if(y*k >= x) cnt += (n/k*x);
+    else if(val) cnt += n*y;
+    else cnt += (n-k)*y + x;
+    return cnt;
+}
+
 void solve(){
-    ll n, k;
-    cin >> n >> k;
-    ll a[n][n];
-    for(ll i = 0; i < n; i++)  for(ll j = 0; j < n; j++) a[i][j] = 0;
-    ll x = 0;
-    for(ll i = 0; i < n; i++){
-        if(x >= k) break;
-        for(ll j = 0; j < n; j++) {
-                 if(x >= k) break;
-                 a[j][(i+j)%n] = 1, x++;
+    ll n, m, x, k, y;
+    cin >> n >> m >> x >> k >> y;
+    ll a[n], b[m];
+    for(ll i = 0; i < n; i++) cin >> a[i];
+    for(ll i = 0; i < m; i++) cin >> b[i];
+    ll prev = -1;
+    vector<ll> v;
+    ll cnt = 0, ki = 0;
+    for(ll i = 0; i < n; i++) {
+        if(a[i] != b[ki]) v.push_back(a[i]);
+        else{
+            ll ma = max(prev, a[i]), xi = check(v, ma, x, k, y);
+            if(xi == -1){
+                cout << -1 << endl;
+                return;
+            } else  cnt+=xi;
+            v.clear();
+            prev = a[i], ++ki;
         }
     }
-    ll xmin = 999999999999, xmax = 0, ymin = 99999999999, ymax = 0, cnt = 0;
-    for(ll i = 0; i < n; i++){
-        ll x = 0;
-        for(ll j = 0; j < n; j++) a[i][j] ? ++x:0;
-        xmin = min(x, xmin);
-        xmax = max(x, xmax);
-    }
-    for(ll i = 0; i < n; i++){
-        ll x = 0;
-        for(ll j = 0; j < n; j++) a[j][i] ? ++x:0;
-        ymin = min(x, ymin);
-        ymax = max(x, ymax);
-    }
-    cnt = (xmin - xmax)*(xmin - xmax) + (ymin - ymax)*(ymin - ymax);
-    
-    cout << xmin << " " << xmax << " | " << ymin << " " << ymax << endl;
-    cout << cnt << endl;
-    for(ll i = 0; i < n; i++){
-        for(ll j = 0; j < n; j++) cout << a[i][j];
-            cout << endl;
+    if(ki < m) cout << -1 << endl;
+    else{
+        ll xi = check(v, prev, x, k, y);
+        if(xi == -1){
+            cout << -1 << endl;
+            return;
+        } else  cnt+=xi;
+        cout << cnt << endl;
     }
 }
 
@@ -42,57 +53,7 @@ void solve(){
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(0);
-  int t;cin>>t;while(t--)
-  solve();
-  return 0;
-}
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long int ll;
-
-void solve(){
-    ll n, k;
-    cin >> n >> k;
-    ll a[n][n];
-    for(ll i = 0; i < n; i++)  for(ll j = 0; j < n; j++) a[i][j] = 0;
-    ll x = 0;
-    for(ll i = 0; i < n; i++){
-        for(ll j = i; j < n; j++) {
-            //cout << j << " " << j-i << " " << x <<  endl;
-                 if(x >= k) break;
-                 a[j][j-i] = 1, x++;
-                 if(x >= k) break;
-                 if( i != 0)a[j-i][j] = 1, x++;
-        }
-        if(x >= k) break;
-    }
-    ll xmin = 999999999999, xmax = 0, ymin = 99999999999, ymax = 0, cnt = 0;
-    for(ll i = 0; i < n; i++){
-        ll x = 0;
-        for(ll j = 0; j < n; j++) a[i][j] ? ++x:0;
-        xmin = min(x, xmin);
-        xmax = max(x, xmax);
-    }
-    for(ll i = 0; i < n; i++){
-        ll x = 0;
-        for(ll j = 0; j < n; j++) a[j][i] ? ++x:0;
-        ymin = min(x, ymin);
-        ymax = max(x, ymax);
-    }
-    cnt = (xmin - xmax)*(xmin - xmax) + (ymin - ymax)*(ymin - ymax);
-    cout << xmin << " " << xmax << " | " << ymin << " " << ymax << endl;
-    cout << cnt << endl;
-    for(ll i = 0; i < n; i++){
-        for(ll j = 0; j < n; j++) cout << a[i][j];
-            cout << endl;
-    }
-}
-
-
-int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  int t;cin>>t;while(t--)
+  //int t;cin>>t;while(t--)
   solve();
   return 0;
 }
