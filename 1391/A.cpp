47b0e3052a19364314c02225ad1064c0ae0c4407
Basukinath Tiwari <basukinathtiwari231198@gmail.com>
2020-08-10 17:13:30 +0530

    Create A.cpp

---
+++ 1391/A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void solve(){
+    ll n;
+    cin >> n;
+    for(int i = 1; i <= n; ++i) cout << i << " ";
+    cout << endl;
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int t;cin>>t;while(t--)
+  solve();
+  return 0;
+}
