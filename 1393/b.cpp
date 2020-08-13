153dc093a8a54c28e33fcd17c09b7550ce5da83b
Basukinath Tiwari <basukinathtiwari231198@gmail.com>
2020-08-13 12:53:24 +0530

    Update b.cpp

--- 1393/b.cpp
+++ 1393/b.cpp
@@ -3,23 +3,46 @@ using namespace std;
 typedef long long int ll;
 
 void solve(){
-    ll n;
+    int n, q;
     cin >> n;
-    ll a[n], d[n];
-    for(ll i = 0; i < n; i++) cin >> a[i], d[i] = 99999999999999;
-    ll x = 99999999999999;
-    for(ll i = 0; i < n; i++) {
-        if(a[i] == 0) x = i;
-        d[i] = min(d[i], abs(x - i));
+    int a[n], mp[n+1] = {0};
+    for(int i = 0; i < n; i++) cin >> a[i], mp[a[i]]++;
+    multiset<int> ms;
+    ms.insert(0);
+    ms.insert(0);
+    ms.insert(0);
+    cin >> q;
+    for(int i = 0; i <= n; i++) ms.insert(mp[i]);
+    while (q--)
+    {
+        char c; int x;
+        cin >> c >> x;
+        ms.erase(ms.find(mp[x]));
+        c == '+' ? ++mp[x]: --mp[x];
+        ms.insert(mp[x]);
+        auto it = ms.rbegin();
+        int val = *it;
+        if(val < 4){
+            cout << "no" << endl;
+            continue;
+        } else if(val >= 8){
+            cout << "yes" << endl;
+            continue;
+        }
+        ++it;
+        int val2 = *it;
+        ++it;
+        int val3 = *it;
+        if(val <= 5) {
+                if(val2 >= 4 || (val2 >= 2 && val3 >= 2)) cout << "YES" << endl;
+                else cout << "NO" << endl;
+        } else {
+            if(val2 >= 2) cout << "YES" << endl;
+            else cout << "NO" << endl;
+        }
     }
-    x = 99999999999999;
-    for(ll i = n-1; i >= 0; i--) {
-        if(a[i] == 0) x = i;
-        d[i] = min(d[i], abs(x - i));
-    }
-    for(ll i = 0; i < n; i++) cout << d[i] << " ";
+    
 }
-
 
 int main(){
   ios_base::sync_with_stdio(false);
