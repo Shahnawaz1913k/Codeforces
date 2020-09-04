0bc61f51a7d98d50cab035db6364601021035ba5
Basukinath Tiwari <basukinathtiwari231198@gmail.com>
2020-09-04 19:27:11 +0530

    Update atcoda.cpp

--- atcoda.cpp
+++ atcoda.cpp
@@ -1,39 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-
-void solve(int x){
-    ll n;
-    cin >> n;
-    string in, out;
-    cin >> in >> out;
-    string s[n];
-    for(ll i = 0; i < n; i++){
-        for(ll j = i; j < n; j++){
-            if(i == j) {
-                //s[i][j] = (out[i] == 'Y' ? 'Y':'N');
-                s[i][i] = 'Y';
-                continue;
-            }
-            s[i][j] = (out[j-1] == 'Y' && s[i][j-1] == 'Y' && in[j] == 'Y' ? 'Y':'N');
-        }
-        for(ll j = i-1; j >= 0; --j){
-            s[i][j] = (out[j+1] == 'Y' && s[i][j+1] == 'Y' && in[j] == 'Y' ? 'Y':'N');
-        }
+int mod = 998244353 ;
+void solve(){
+  ll n ;
+  cin >> n ;
+  ll a[n];
+  for(ll i = 0 ; i < n ; i++)cin >> a[i];
+  sort(a,a+n);
+  ll cnt = 0,ans = 1e18;
+  ll val = pow(2, log2(a[n-1])*1.0/(n-1));
+  // while (log2(a[n-1])*1.0/n-1 == log2(val))val++;
+  for(ll i = max(1ll, val-2); i <= val+1; i++)
+  {
+    cnt = 0;
+    for(ll j = 0 ; j < n ; j++)
+    {
+      cnt+=abs(pow(i,j) - a[j]);
+      
     }
-    cout << "Case #"<<x+1<<":"<<endl;
-    for(ll i = 0; i < n; i++){
-        for(ll j = 0; j < n; j++) cout << s[i][j];
-        cout << endl;
-    }
+    // cout << i << " " << cnt << endl;
+    ans = min(ans,cnt);
+  }
+  // cout << val << endl;
+  cout << ans ;
 }
-
+ 
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(0);
-  int t;cin>>t;
-  //while(t--)
-  for(int i = 0; i < t; i++)
-  solve(i);
+ //  int t;cin>>t;for(int i = 0 ;i<t;i++)
+  solve();
+   
   return 0;
 }
