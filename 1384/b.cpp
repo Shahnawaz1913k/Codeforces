2ce2e017909a5107fed87326155afc0f9b5243c3
Rakesh kumar Tiwari <rakeshkumartiwari11491981@gmail.com>
2020-10-03 10:25:58 +0530

    Update b.cpp
    Fast solution

--- 1384/b.cpp
+++ 1384/b.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 typedef long long int ll;
 
-void solve(){
+/*void solve(){
     ll n, k, l;
     cin >> n >> k >> l;
     ll a[n], t[2*k];
@@ -31,11 +31,44 @@ void solve(){
     }
     cout << "yes" << endl;
 }
-
+*/
 int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  int t;cin>>t;while(t--)
-  solve();
-  return 0;
+  ios_base::sync_with_stdio(0), cin.tie(0);
+
+	int test;
+	cin >> test;
+	while (test--)
+	{
+		int n, k, l;
+		cin >> n >> k >> l;
+		vector<int> d(n+2, -k);
+		for (int i = 1; i <= n; ++i)
+			cin >> d[i];
+
+		set<tuple<int, int, bool>> mark;
+		function<bool(int, int, bool)> go = [&](int pos, int tide, bool down)
+		{
+			if (pos > n) return true;
+			
+			if (mark.find({ pos, tide, down }) != mark.end())
+				return false;
+
+			mark.insert({ pos, tide, down });
+
+			tide += down ? -1 : +1;
+			if (tide == 0) down = false;
+			if (tide == k) down = true;
+
+			if (d[pos] + tide <= l && go(pos, tide, down))
+				return true;
+			if (d[pos + 1] + tide <= l && go(pos + 1, tide, down))
+				return true;
+			return false;
+		};
+
+		if (go(0, 0, false)) cout << "Yes\n";
+		else cout << "No\n";
+	}
+
+	return 0;
 }
